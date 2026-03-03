5464691c1d65a53d719a9c684bcfd3b9b7a0a7c8
CJPawlus <chris.john.pawlus@icloud.com>
2026-03-03 11:19:08 -0500

    Added introduction
    I added my intro from Piazza into my code. Before this though I made sure it clarified that this is my intro from the beginning of class when it prints.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -10,7 +10,9 @@ def main():
 
     name = "Christopher Pawlus" # TODO: Insert your name between the double quotes
 
-    print(f"{name}, Welcome to the CSS course!")
+    introduction= "Hello everyone! My name is Christopher Pawlus, a Junior at Andover High School in Andover Massachusetts. I've lived here for most of my life, excluding a one year stay in Oregon. My favorite subject in school depends on the day but it's always either a science or math class. As of writing this I like statistics the most because right now our unit is blending what we have been learning all year into one procedure! Outside of school my favorite activity is either playing with friends or doing anything that is related to engineering like robotics (proud ARC Thunder member here) or personal projects. I would say something that is unique about me is my current engineering project in which I am designing and prototyping a mono-wing aircraft that utilizes a custom flap control system to explore unconventional solutions to flight control."
 
+    print(f"{name}, Welcome to the CSS course!")
+    print(f"This is your introduction from the begining of the class: {introduction}")
 if __name__ == "__main__":
     main()
